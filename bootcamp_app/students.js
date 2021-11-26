fdc7783ea489bb3fd61258ff604c694075623ca6
Abdullah Habes <abdulla096@hotmail.com>
2021-11-26 01:32:44 -0500

    add node pg queries

---
+++ bootcamp_app/students.js
@@ -0,0 +1,26 @@
+const {Pool} = require('pg');
+
+const pool = new Pool({
+    user: 'vagrant',
+    password: '123',
+    host: 'localhost',
+    database: 'bootcampx'
+  }
+);
+
+const cohort =  process.argv[2];
+const limit = process.argv[3];
+console.log(cohort)
+pool.query(`
+    SELECT students.id, students.name, cohorts.name as cohort_name
+    FROM students
+             JOIN cohorts ON students.cohort_id = cohorts.id
+    WHERE cohorts.name LIKE '%${cohort}%'
+    LIMIT ${limit};
+`)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`);
+    })
+  })
+  .catch(err => console.error('query error', err.stack));
